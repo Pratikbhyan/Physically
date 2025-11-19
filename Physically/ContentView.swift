75a861f00c1c5ae1a445e6cfb5d7af22a76cba86
Pratikbhyan <pkbh247@gmail.com>
2025-11-19 06:21:02 +0530

    Initial Commit

---
+++ Physically/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Physically
+//
+//  Created by Pratik Bhyan on 19/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Physically/PhysicallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicallyApp.swift
+//  Physically
+//
+//  Created by Pratik Bhyan on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PhysicallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
